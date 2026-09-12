a540740421ba32c28cee7123824e4a92970676b0
Vocs Ong <omega.ong@gmail.com>
2026-09-12 17:14:25 +0800

    Make the sidebar folder font and Vocs Code brand bigger

--- src/renderer/src/components/Sidebar.tsx
+++ src/renderer/src/components/Sidebar.tsx
@@ -197,7 +197,7 @@ export function Sidebar() {
     <aside className="sidebar">
       <div className="sidebar-top">
         <div className="brand">
-          <Icon name="logo" size={18} />
+          <Icon name="logo" size={22} />
           <span>Vocs Code</span>
         </div>
         <Button variant="primary" size="sm" icon="folder" onClick={() => void startNewSession()} title="New folder (Ctrl+N)">
